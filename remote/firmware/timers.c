2ca32e9194b126df38ed88b77d713b6d9b28faff
Steven Parker <steven.parker@oit.edu>
2022-01-23 00:17:09 -0800

    Move timers to separate module

---
+++ remote/firmware/timers.c
@@ -0,0 +1,37 @@
+
+#include <timers.h>
+
+void tmr0_init(void) {
+    // T0CON0
+    //   EN: Enable(1)/Disable(0) timer 0
+    //   MD16: Enable(1)/Disable(0) 16-bit mode
+    //   OUTPS: Post-scaler ratio
+
+
+    T0CON0bits.EN = 0;
+    T0CON0bits.MD16 = 0;
+    T0CON0bits.OUTPS = 0xf;
+
+    // T0CON1
+    //   CS: Clock source
+    //   ASYNC: Async(1)/Sync(0)
+    //   CKPS: Pre-scaler ratio
+    T0CON1bits.CS = 2;
+    T0CON1bits.ASYNC = 0;
+    T0CON1bits.CKPS = 0x8;
+
+    // TMR0H: TMR0 MSB
+    // TMR0L: TMR0 LSB
+    TMR0H = 0x02;
+    TMR0L = 0x00;
+
+    // IPR3
+    //   TMR0IP: High(1)/Low(0) priority
+    IPR3bits.TMR0IP = 0;
+
+    // PIE3
+    //   TMR0IE: Enable(1)/Disable(0) interrupt
+    PIE3bits.TMR0IE = 1;
+
+    T0CON0bits.EN = 1;
+}
---
+++ remote/firmware/timers.h
@@ -0,0 +1,10 @@
+ 
+#ifndef TIMERS_H
+#define	TIMERS_H
+
+#include <xc.h> 
+
+
+
+#endif	/* TIMERS_H */
+
